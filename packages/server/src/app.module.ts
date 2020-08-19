15c4e2ce561ec84be1dd4aae38dd01f0bf470d83
qianjiacheng <qianjiacheng@mchz.com.cn>
2020-08-19 22:17:05 +0800

    feat: server子模块新增gql模块

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { RecipesModule } from './recipes/recipes.module';
+
 @Module({
-  imports: [GraphQLModule.forRoot({})],
+  imports: [
+    RecipesModule,
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      installSubscriptionHandlers: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
